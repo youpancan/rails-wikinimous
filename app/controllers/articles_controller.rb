8281dcac2a1f4d04535638f2899e46ed1b0d2e6e
YOU PAN <sunnypanyou@gmail.com>
2023-02-21 22:10:14 -0500

    didn't code front-end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,13 +24,14 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    @article.update
+    @article.update(article_params)
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    redirect_to articles_path, status: :see_other
   end
 
   private
